4a12c0ca152d20fce4cadc6312d7712dba4a534c
Martin Salmeron <140337045+Msalmeron95@users.noreply.github.com>
2024-08-24 21:32:45 -0400

    PracticaUno

---
+++ PracticaUno.kt
@@ -0,0 +1,40 @@
+fun main() {
+    println("Ingrese el primer número:")
+    val numeroUno = readLine()?.toDoubleOrNull()
+
+    println("Ingrese el segundo número:")
+    val numeroDos = readLine()?.toDoubleOrNull()
+
+    println("Ingrese la operación (+, -, *, /, ^):")
+    val operacion = readLine()
+
+    if (numeroUno == null || numeroDos == null || operacion.isNullOrBlank()) {
+        println("Error: Ingrese números válidos y una operación válida.")
+        return
+    }
+
+    // Check if any input number is zero and throw an error
+    if (numeroUno == 0.0 || numeroDos == 0.0) {
+        println("Error: Uno de los números ingresados es cero.")
+        return
+    }
+
+    val resultado = when (operacion) {
+        "+" -> numeroUno + numeroDos
+        "-" -> numeroUno - numeroDos
+        "*" -> numeroUno * numeroDos
+        "/" -> {
+            if (numeroDos == 0.0) {
+                println("Error: División por cero no permitida.")
+                return
+            }
+            numeroUno / numeroDos
+        }
+        "^" -> Math.pow(numeroUno, numeroDos)
+        else -> {
+            println("Operación no reconocida.")
+            return
+        }
+    }
+    println("El resultado de la operación $numeroUno $operacion $numeroDos es $resultado")
+}
